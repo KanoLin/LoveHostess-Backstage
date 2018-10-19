923a802a341d843502da737e9383550d7ebbfeb7
KanoLin <fordreams@outlook.com>
2018-10-19 23:08:05 +0800

    Update create.sql

--- create.sql
+++ create.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS hostess(
     `grade`          VARCHAR(20)       NOT NULL DEFAULT '' COMMENT '选手年级',
     `academy`        VARCHAR(50)       NOT NULL DEFAULT '' COMMENT '选手学院',
     `tele`           VARCHAR(20)       NOT NULL DEFAULT '' COMMENT '选手电话',
-    `time`           DATETIME          NOT NULL            COMMENT '选手海选时间',    
+    `time`           VARCHAR(255)      NOT NULL DEFAULT '' COMMENT '选手海选时间',    
     `created_at`     DATETIME          NOT NULL            COMMENT '创建时间',
     `updated_at`     DATETIME          NOT NULL            COMMENT '修改时间'
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='选手表';
