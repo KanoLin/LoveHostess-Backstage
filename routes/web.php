862e9adf1e917ffbd4cfc37c64e4342bd41c25ae
KanoLin <fordreams@outlook.com>
2018-11-16 22:33:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,17 @@ use Illuminate\Support\Facades\Response;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::any('/date',functIon() {
+Route::any('/date', function () {
     return date('Y-m-d');
 });
 
-Route::any('/testLogin',function(Request $request){
+Route::any('/testLogin', function (Request $request) {
     $request->session()->put([
         'openid' => 'AHKDHFOAHIHFPOIWQHOIHFOEWFOISJDAmmF',
         'nickname' => 'test',
@@ -29,29 +29,36 @@ Route::any('/testLogin',function(Request $request){
     return 'login success';
 })->middleware('web');
 
-Route::any('/testLogout',function(Request $request){
+Route::any('/testLogout', function (Request $request) {
     $request->session()->flush();
     return 'logout success';
 })->middleware('web');
 
 
-Route::post('/api/signUp','LoveHostessRegister@register')->middleware('validate');
+Route::post('/api/signUp', 'LoveHostessRegister@register')->middleware('validate');
 
-Route::middleware('web')->group(function(){
-    Route::post('/api/Info','LoveHostessVote@Info');
-    Route::post('/api/Vote','LoveHostessVote@Vote')->middleware('login');
+Route::post('/api/checkLogin', function () {
+    return response()->json([
+        'err_code' => 0,
+        'err_msg' => ''
+    ]);
+})->middleware('login');
+Route::middleware('web')->group(function () {
+    Route::post('/api/Info', 'LoveHostessVote@Info');
+    Route::post('/api/Vote', 'LoveHostessVote@Vote')->middleware('login');
 });
 
-Route::get('/img/{img_name}',function($name){
-    return response()->file(storage_path('app/public').'/img/'.$name,['Content-type'=>'image/png']);
+Route::get('/img/{img_name}', function ($name) {
+    return response()->file(storage_path('app/public') . '/img/' . $name, ['Content-type' => 'image/png']);
 });
 
-Route::get('/aud/{aud_name}',function($name){
-    $size=filesize(storage_path('app/public').'/aud/'.$name);
-    return response()->file(storage_path('app/public').'/aud/'.$name,[
-            'Content-type'=>'audio/mp3',
-            'Content-Disposition'=>'inline;filename="'.$name.'"',
-            'Accept-Ranges'=>'0-'.($size-1),
-            'Content-Length'=>$size,
+
+Route::get('/aud/{aud_name}', function ($name) {
+    $size = filesize(storage_path('app/public') . '/aud/' . $name);
+    return response()->file(storage_path('app/public') . '/aud/' . $name, [
+        'Content-type' => 'audio/mp3',
+        'Content-Disposition' => 'inline;filename="' . $name . '"',
+        'Accept-Ranges' => '0-' . ($size - 1),
+        'Content-Length' => $size,
     ]);
 });
